aca48aebc7b8e6e6bfc5ae16fb7729aa46294e72
Denise Albrecht <denise-albrecht@posteo.de>
2025-01-14 15:51:12 +0100

    Introduce state to handle confirmation message & reuse css role for confirm question / Part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { initialColors } from "./lib/colors";
 
-// import Color from "./Components/Color/Color";
 import ColorForm from "./Components/ColorForm/ColorForm.jsx";
 import ColorCards from "./Components/ColorCards/ColorCards.jsx";
 
@@ -14,12 +13,17 @@ function App() {
     setColors((prevColors) => [newColor, ...prevColors]);
   }
 
+  function handleDeleteColor(colorId) {
+    setColors((prevColors) =>
+      prevColors.filter((color) => color.id !== colorId)
+    );
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
-
       <ColorForm onAddColor={handleAddColor} />
-      <ColorCards colors={colors} />
+      <ColorCards colors={colors} onDelete={handleDeleteColor} />
     </>
   );
 }
